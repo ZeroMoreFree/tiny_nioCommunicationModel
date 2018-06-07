4fb403d363f15faee7de95036560ae36bda63026
zheng <764397816@qq.com>
2018-06-07 15:13:06 +0800

    Update TcpServer.java

--- com/zero/nio/demo/TcpServer.java
+++ com/zero/nio/demo/TcpServer.java
@@ -1,4 +1,5 @@
-package com.faisco.niodemo;
+package com.zero.nio.demo;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -10,107 +11,118 @@ import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
 
-/**
- * @author chengsh05
- *
- */
-public class TcpServer {
+public class TcpServer{
 
-    public static void startServer(int port) throws IOException{
-        /*
-         *开启一个服务channel，
-         *A selectable channel for stream-oriented listening sockets.
-         */
-        ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        serverChannel.configureBlocking(false);
-        serverChannel.bind(new InetSocketAddress(port));
+    private ServerSocketChannel serverSocket ;
+    private Selector selector;
 
-        /*
-         * 创建一个selector
-         */
-        Selector selector = Selector.open();
-        /*
-         * 将创建的serverChannel注册到selector选择器上，指定这个channel只关心OP_ACCEPT事件
-         */
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+    public TcpServer(int port) throws Exception{
 
-        while (true) {
-            /*
-             * select()操作，默认是阻塞模式的，即，当没有accept或者read时间到来时，将一直阻塞不往下面继续执行。
-             */
-            int readyChannels = selector.select();
-            if (readyChannels <= 0) {
+        serverSocket = ServerSocketChannel.open();
+        serverSocket.configureBlocking(false);
+        serverSocket.bind(new InetSocketAddress(port));
+
+        selector = Selector.open();
+        serverSocket.register(selector,SelectionKey.OP_ACCEPT);
+
+    }
+
+    public void start() throws Exception{
+
+        while (true){
+
+            //判断是否有就绪的channel
+            if(selector.select() <= 0){
                 continue;
             }
 
-            /*
-             * 从selector上获取到了IO事件，可能是accept，也有可能是read
-             */
-            Set<SelectionKey> SelectonKeySet = selector.selectedKeys();
-            Iterator<SelectionKey> iterator = SelectonKeySet.iterator();
+            Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+            Iterator<SelectionKey> it = selectionKeySet.iterator();
 
-            /*
-             * 循环遍历SelectionKeySet中的所有的SelectionKey
-             */
-            while (iterator.hasNext()) {
-                SelectionKey key = iterator.next();
-                if (key.isAcceptable()) {          //处理OP_ACCEPT事件
-                    SocketChannel socketChannel = serverChannel.accept();
-                    socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                } else if (key.isReadable()) {  //处理OP_READ事件
-                    SocketChannel socketChannel = (SocketChannel) key.channel();
-                    StringBuilder sb = new StringBuilder();
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            while (it.hasNext()){
 
-                    int readBytes = 0;
-                    int ret = 0;
-                    /*
-                     * 注意读数据的时候，ByteBuffer的操作，需要flip,clear进行指针位置的调整
-                     */
-                    while ((ret = socketChannel.read(byteBuffer)) > 0) {
-                        readBytes += ret;
-                        byteBuffer.flip();
-                        sb.append(Charset.forName("UTF-8").decode(byteBuffer).toString());
-                        byteBuffer.clear();
-                    }
+                SelectionKey selectionKey = it.next();
+                if(selectionKey.isAcceptable()){//查看serverSocket是否已经接收到socket了，有的话，则将接收到的socket注册到selector中，兴趣为读
+                    checkHasAcceptedSocketAndDoRegister();
+                }else if(selectionKey.isReadable()){//查看接收到的socket是否准备好了被读
 
-                    if (readBytes == 0) {
-                        System.err.println("handle opposite close Exception");
-                        socketChannel.close();
-                    }
+                    SocketChannel socket = (SocketChannel) selectionKey.channel();
 
-                    String message = sb.toString();
-                    System.out.println("Message from client: " + message);
-                    if ("cq".equalsIgnoreCase(message.toString().trim())) {
-                        System.out.println("Client is going to shutdown!");
-                        socketChannel.close();
-                    } else if ("serverQuit".equalsIgnoreCase(message.trim())) {
-                        System.out.println("Server is going to shutdown!");
-                        socketChannel.close();
-                        serverChannel.close();
-                        selector.close();
-                        System.exit(0);
-                    } else {
-                        String outMessage = "Server response：" + message;
-                        socketChannel.write(Charset.forName("UTF-8").encode(outMessage));
+                    StringBuffer message = readMessageFromClient(socket);
+                    System.out.println("Receving Message from client:" + message);
+
+                    if(receivingCloseClientInstructions(message.toString())){
+                        closeClient(socket);
+                    }else if(receivingCloseServerInstructions(message.toString())){
+                        closeServer(socket);
+                    }else{
+                        responseClient(socket);
                     }
                 }
-                /*
-                 * 将selector上当前已经监听到的且已经处理了的事件标记清除掉。
-                 */
-                iterator.remove();
+                //已经处理过的，则移出SelectionKeySet
+                it.remove();
             }
         }
     }
 
-    /**
-     * @param args
-     */
+    private void checkHasAcceptedSocketAndDoRegister() throws IOException {
+        SocketChannel socket = serverSocket.accept();
+        if(socket != null){
+            socket.configureBlocking(false);
+            socket.register(selector,SelectionKey.OP_READ);
+        }
+    }
+
+    private StringBuffer readMessageFromClient(SocketChannel socket) throws IOException {
+        ByteBuffer readFromSocketBuffer = ByteBuffer.allocate(1024);
+        int bytesRead = socket.read(readFromSocketBuffer);
+        StringBuffer message = new StringBuffer();
+        while (bytesRead >0){
+            readFromSocketBuffer.flip();
+            message.append(Charset.forName("UTF-8").decode(readFromSocketBuffer));
+            readFromSocketBuffer.clear();
+            bytesRead = socket.read(readFromSocketBuffer);
+        }
+        return message;
+    }
+
+    private boolean receivingCloseClientInstructions(String message){
+        return TcpConstants.CLIENT_CLOSE.equalsIgnoreCase(message.trim());
+    }
+
+    private void closeClient(SocketChannel channel) throws Exception{
+        System.out.println("The client is going to shutdown!");
+        channel.close();
+    }
+
+    private boolean receivingCloseServerInstructions(String message){
+        return TcpConstants.SERVER_CLOSE.equalsIgnoreCase(message.trim());
+    }
+
+    private void closeServer(SocketChannel channel) throws Exception{
+        System.out.println("The server is going to shutdown!");
+        channel.close();
+        selector.close();
+        serverSocket.close();
+        System.exit(0);
+    }
+
+    private void responseClient(SocketChannel socket){
+        String serverResponse = "Thanks for your request!";
+        try {
+            socket.write(Charset.forName("UTF-8").encode(serverResponse));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Response client fail!");
+        }
+
+    }
+
     public static void main(String[] args) {
         try {
-            startServer(9999);
-        } catch (IOException e) {
+            TcpServer server = new TcpServer(TcpConstants.SERVER_PORT);
+            server.start();
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
