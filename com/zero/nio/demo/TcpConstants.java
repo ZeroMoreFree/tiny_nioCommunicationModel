5fcc6f6a970100e58a70ec7ad22af9257bcda1c8
zheng <764397816@qq.com>
2018-06-07 15:14:28 +0800

    Create TcpConstants.java

---
+++ com/zero/nio/demo/TcpConstants.java
@@ -0,0 +1,11 @@
+package com.zero.nio.demo;
+
+public class TcpConstants {
+
+    public final static String CLIENT_CLOSE = "cq";
+    public final static String SERVER_CLOSE = "sq";
+    public final static String SERVER_HOSTNAME = "localhost";
+    public final static int SERVER_PORT = 9999;
+
+
+}
